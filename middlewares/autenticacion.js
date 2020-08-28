43b78a35859f8fb8e162462ccbef4e6625851888
Andres <andres.buitrago@puxles.com>
2020-08-28 08:25:37 -0500

    revsion redis

--- middlewares/autenticacion.js
+++ middlewares/autenticacion.js
@@ -3,14 +3,21 @@ const RedisClouster = require("redis-clustr")
 const HOST_REDIS = process.env.HOST_REDIS;
 const PORT_REDIS = process.env.PORT_REDIS;
 const redis = new RedisClouster({
-    createClient: function(HOST_REDIS, PORT_REDIS) {
-        return RedisClient.createClient(HOST_REDIS, PORT_REDIS)
+    servers: [{
+        host: HOST_REDIS,
+        port: PORT_REDIS
+    }],
+    createClient: function(port, host) {
+        return RedisClient.createClient(port, host)
     }
 
 });
 
 redis.on("error", function(error) {
     console.error(error);
+});
+redis.subscribe('my-channel', function(err) {
+    redis.publish('my-channel', 'have a lovely day!');
 });
 
 let verifyUser = async(req, res, next) => {
